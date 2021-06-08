b45afea76e0a21fe7915e962f558a2b531196bc6
Zachary Zensen <TechnomagezZ@yahoo.com>
2021-06-08 16:46:39 -0700

    updated

--- src/main/java/module5/MovieDB.java
+++ src/main/java/module5/MovieDB.java
@@ -1,7 +1,20 @@
 package module5;
 
-public class MovieDB {
-	public static void main(String[] args) {
-		MovieQueries example = new MovieQueries();
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MovieDB extends Application{
+	public void start(Stage primaryStage) throws Exception{
+		Parent root = FXMLLoader.load(getClass().getResource("MovieDB.fxml"));
+		primaryStage.setTitle("Movie Database");
+		primaryStage.setScene(new Scene(root, 407, 600));
+		primaryStage.show();
+	}
+	
+	public static void main(String Args[]) {
+		launch(Args);
 	}
 }
